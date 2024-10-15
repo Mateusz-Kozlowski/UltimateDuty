a02452f078aef37449624dda9d4e2df2301d1db8
Mateusz Kozlowski <mateuszkozlowski1124@gmail.com>
2024-10-15 12:07:58 +0200

    Implement login page
    Author:    Mateusz-Kozlowski <mateuszkozlowski1124@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 
-//import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/auth/SignUpPage";
 import toast, { Toaster } from "react-hot-toast";
